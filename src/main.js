0a53bb83562d4981344a68240cd7a055e53757c5
Luffytaro22 <manuelsanchez059@gmail.com>
2025-08-19 17:22:39 -0500

    Added a typewriter animation

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ document.querySelector('#app').innerHTML = `
   <div>
     <div id="overlay"></div>
     <section id="gretting">
-    <h1>WELCOME TO THE WORD-STRESS GAME!</h1>
+    <h1 class="typewriter">WELCOME TO THE WORD-STRESS GAME!</h1>
     <h2>PRESS START!</h2>
-    <button id="start-button">
+    <button id="start-button" class="hide">
       <p>START</p>
     </button>
     </section>
@@ -38,3 +38,17 @@ await getRandomWord();
 hyphenWord(document.getElementById("word").textContent);
 pressContinueButton();
 pressRestartButton();
+
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const startBn = document.getElementById("start-button");
+
+h1.addEventListener('animationend', () => {
+  h2.classList.add("typewriter");
+  h2.style.animation = "typing-h2 1.5s steps(30, end) forwards, blink-caret .75s step-end infinite";
+  h1.style.borderRight = "none";
+});
+
+h2.addEventListener('animationend', () => {
+  startBn.classList.remove("hide");
+});
