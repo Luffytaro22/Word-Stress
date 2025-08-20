29799a78136ce16a4757bcfb6687b84288e918d9
Luffytaro22 <manuelsanchez059@gmail.com>
2025-08-19 20:56:10 -0500

    Modified syllables separator

--- src/js/showWordSyllables.js
+++ src/js/showWordSyllables.js
@@ -1,22 +1,23 @@
-import nlp from 'compromise';
-import plg from 'compromise-speech'
+import nlp from 'compromise'
+import speechPlugin from 'compromise-speech'
 import { checkStress } from './selectSyllable';
 import { selectedWord } from '../main';
-nlp.extend(plg)
+nlp.plugin(speechPlugin)
 
 export async function hyphenWord(word) {
   //Hyphen the word
-  const result = await nlp(word);
+  const result = nlp(word);
+  
   //Obtain each syllable
-  const wordSyllables = result.syllables()[0];
+  const wordHyphen = result.syllables()[0];
   const container = document.getElementById("syllables-container");
   container.innerHTML = '';
   //Insert each syllable in the button items.
-  for(let i = 0; i < wordSyllables.length; i++){
+  for(let i = 0; i < wordHyphen.length; i++){
     
     const syllablePlaceholder = document.createElement("button");
     syllablePlaceholder.className = "syllable";
-    syllablePlaceholder.textContent = wordSyllables[i];
+    syllablePlaceholder.textContent = wordHyphen[i];
     syllablePlaceholder.addEventListener('click', () => checkStress(selectedWord, i));
     container.appendChild(syllablePlaceholder);
   }
